596d51c67bb934331e842b88c2798cb166f54732
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-07-17 22:10:38 +0600

    deploy error fiexed

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.DEV
     ? "http://localhost:3000/api/v1"
-    : "hhttps://secure-nest-server.vercel.app/api/v1",
+    : "https://secure-nest-server.vercel.app/api/v1",
   withCredentials: true,
 });
 
