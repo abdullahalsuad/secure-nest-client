ec8200c001680ae4ae50b842949c677e59075c83
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-07-17 21:04:29 +0600

    backed live API added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,9 @@ import axios from "axios";
 // import { AuthContext } from "../context/AuthProvider";
 
 const instance = axios.create({
-  baseURL: import.meta.env.DEV ? "http://localhost:3000/api/v1" : "",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:3000/api/v1"
+    : "https://secure-nest-server.vercel.app/api/v1",
   withCredentials: true,
 });
 
